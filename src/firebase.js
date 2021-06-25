90f7086868c31857b2470cb237cad1daeed60251
Thinley Dorji <thinleyjimmy123@gmail.com>
2021-06-25 14:30:52 +0600

    Starter project
    Starter project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAdQr68-v_QWdO009MzefrXAp6ZL3V6Y5M",
+  authDomain: "clone-e69c6.firebaseapp.com",
+  projectId: "clone-e69c6",
+  storageBucket: "clone-e69c6.appspot.com",
+  messagingSenderId: "211295453205",
+  appId: "1:211295453205:web:d798289a78e36417a14e1c",
+};
